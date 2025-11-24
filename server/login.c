8a1cd0f4d20c415287b623a8093302b48fa21c64
Malenkov Mikhail Sergeevich <mike.malenkov@gmail.com>
2025-11-24 18:55:35 +0300

    Made the test server code work with Apache2.

--- server/login.c
+++ server/login.c
@@ -3,9 +3,6 @@
 
 int main(void)
 {
-  long length = atol(getenv("CONTENT_LENGTH"));
-  printf("Content-Type: text/plain\n");
-  printf("Content-Length: %ld\n\n", length);
-  while (length--) putchar(getchar());
+  printf("Content-Type: text/html\n\nHello World!\n");
 }
 
