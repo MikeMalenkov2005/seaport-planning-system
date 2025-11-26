31b94a4bac1b0340af8197476f6432fcf25c8726
Malenkov Mikhail Sergeevich <mike.malenkov@gmail.com>
2025-11-26 20:17:58 +0300

    Improved testing code.

--- server/login.c
+++ server/login.c
@@ -5,7 +5,7 @@ int main(void)
 {
   char *length_string = getenv("CONTENT_LENGTH");
   long length = length_string ? atol(length_string) : 0;
-  printf("Content-Type: text/plain\n\nMETHOD: %s\nURI: %s\nQUERY: %s\n",
+  printf("Content-Type: text/plain\n\nMETHOD: %s\nURI: %s\nQUERY: %s\nDATA:\n",
       getenv("REQUEST_METHOD"), getenv("REQUEST_URI"), getenv("QUERY_STRING"));
   while (length--) putchar(getchar());
   return EXIT_SUCCESS;
