3354bebcb54d0c1e513fc969cf8d0553375c1448
isupo <isupovaea63@gmail.com>
2025-12-01 16:15:20 +0300

    Добавление скрипта заполнения справочных таблиц

---
+++ full.sql
@@ -0,0 +1,45 @@
+INSERT INTO job_title (name) VALUES
+('Рабочий'),
+('Инженер'),
+('Оператор');
+
+INSERT INTO direction (name) VALUES
+('Север 32'),
+('Юг 337'),
+('Север 433/1');
+
+INSERT INTO transport_way (name) VALUES
+('Sea'),
+('Rail'),
+('Road');
+
+INSERT INTO warehouse_type (name) VALUES
+('Склад'),
+('Холодый склад');
+
+INSERT INTO application_status (name) VALUES
+('Pending'),
+('Approved'),
+('Rejected');
+
+INSERT INTO vessel_type (name) VALUES
+('Грузовое судно'),
+('Танкер');
+
+INSERT INTO vessel_status (name) VALUES
+('В порту'),
+('В пути'),
+('Неисправен');
+
+INSERT INTO equipment_category (name) VALUES
+('Тяжелая техника');
+
+INSERT INTO external_dimensions (external_dimensions_code) VALUES
+('20ft'),
+('40ft'),
+('40ft');
+
+INSERT INTO container_status (name) VALUES
+('Пуст'),
+('Загружен'),
+('Приостановлен');
