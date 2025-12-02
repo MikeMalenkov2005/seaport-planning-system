fd1220d151879919de0f39ed251af6a6becc9929
MikeMalenkov2005 <mike.malenkov@gmail.com>
2025-12-02 16:25:52 +0300

    Made login unique in the database.

--- init_base.sql
+++ init_base.sql
@@ -86,7 +86,7 @@ CREATE TABLE employee (
   surname VARCHAR(60) NOT NULL,
   name VARCHAR(60) NOT NULL,
   patronymic VARCHAR(60),
-  login VARCHAR(60) NOT NULL,
+  login VARCHAR(60) NOT NULL UNIQUE,
   password VARCHAR(60) NOT NULL,
   id_job_title INT NOT NULL,
   PRIMARY KEY (id_employee),
